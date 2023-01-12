9b6eecc1b19e3e796bf3b2aad961daf9d344f423
Widge-5 <77170359+Widge-5@users.noreply.github.com>
2023-01-12 22:09:42 +0000

    Update bb_screenratio.sh
    Version 1.02

--- bb_screenratio.sh
+++ bb_screenratio.sh
@@ -3,7 +3,7 @@
 ######################################################################################################
 ##
 ##  BB9 aspect ratio conversion - by Widge
-##  Version 1.01
+##  Version 1.02
 ##  January 2023
 ##
 ##  # TV modes #
@@ -22,8 +22,10 @@ location="/opt/retropie/configs/"
 filepattern="*.cfg"
 configtxt="/boot/config.txt"
 
-_cQUERY="cCYAN"
-_cOPTIONS="cLCYAN"
+title="BareBones Screen Aspect Adaptation Utility (v1.02)"
+WT_HEIGHT=20
+WT_WIDTH=70
+WT_MENU_HEIGHT=5
 
 function colourecho(){
     _nc="\033[0m"
@@ -48,6 +50,7 @@ function colourecho(){
     esac
   echo -e "${_col}${2}${_nc}"
 }
+
 
 function downloader() {
   rm -rf $2
@@ -81,72 +84,72 @@ function displaymodes () {
   restorechoice="1"
   echo "Your current display is: "
   /opt/vc/bin/tvservice -s
-  colourecho $_cQUERY "What display ratio do you want to convert to?"
-  colourecho $_cOPTIONS "[1] 16:9 (BB9 default, most common)"
-  colourecho $_cOPTIONS "[2] 4:3"
-  colourecho $_cOPTIONS "[3] 5:4 (typical for some Arcade1UP cabinets)"
-  colourecho $_cOPTIONS "Make your selection. Any other key to cancel."
-  read -N1 choice
-    case $choice in
-      1 )
-        echo " : 16:9"
-        displaygroupname="CEA"
-	displaygroup="1"
-        displaymode="4"
-	newoverlay=$overlay169
-	viewportX="173"; viewportY="10"; viewportW="933"; viewportH="700"
-	newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/16x9"
-	colourecho $_cQUERY "You have chosen the default Barebones aspect ratio of 16:9. Would you like to..."
-        colourecho $_cOPTIONS "[1] Just run the local script that strips the bezels and applies global viewport settings for a 16:9 display;"
-	colourecho $_cOPTIONS "[2] Restore the default BareBones config files that were previously stripped out by this script;"
-        colourecho $_cOPTIONS "Make your selection. Any other key to cancel."
-        read -N1 restorechoice
-      ;;
-      2 )
-        echo " : 4:3"
-        displaygroupname="DMT"
-	displaygroup="2"
-        displaymode="16"
-	newoverlay=$overlay43
-	viewportX="9"; viewportY="9"; viewportW="1006"; viewportH="750"
-	newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/4x3"
-      ;;
-      3 )
-        echo " : 5:4"
-        displaygroupname="DMT"
-	displaygroup="2"
-        displaymode="35"
-	newoverlay=$overlay54
-	viewportX="12"; viewportY="41"; viewportW="1256"; viewportH="942"
-	                stretchY="12";                    stretchH="1000"
-	newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/5x4"
-        colourecho $_cQUERY "Almost all of the games available are made with the 4:3 aspect ratio in mind."
-        colourecho $_cQUERY "The display you have selected has a slightly narrower (taller) aspect ratio."
-        colourecho $_cQUERY "Would you like to:"
-        colourecho $_cOPTIONS "[1] Display the games in their true 4:3 aspect ratio; or"
-        colourecho $_cOPTIONS "[2] Distort the games so they stretch to fill the whole screen?"
-        colourecho $_cOPTIONS "Make a selection, or press any other key to quit."
-        read -N1 stretchchoice
-        case $stretchchoice in
-          1 )
-            echo " : Excellent choice."
-          ;;
-          2 )
-            echo " : Distort the image."
-            viewportY=$stretchY
-            viewportH=$stretchH
-          ;;
-          * )
-            echo "  : Cancelled"
-            exit 0
-          ;;
-        esac
-      ;;
-      * )
-        echo "  : Cancelled"
-        exit 0
-      ;;
-    esac
+  choice=$(whiptail --title "$title" --menu "What display ratio do you want to convert to?" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT \
+    "1" "| 16:9 . BB9 default, most common" \
+    "2" "|  4:3 . Classic" \
+    "3" "|  5:4 . Typical for some Arcade1UP cabinets" \
+    3>&1 1>&2 2>&3)
+
+  case $choice in
+    1 ) #16:9
+      displaygroupname="CEA"
+      displaygroup="1"
+      displaymode="4"
+      newoverlay=$overlay169
+      viewportX="173"; viewportY="10"; viewportW="933"; viewportH="700"
+      newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/16x9"
+      restorechoice=$(whiptail --title "$title" --menu \
+        "\nYou have chosen the default Barebones aspect ratio of 16:9.\nWould you like to strip out all of the bezels and apply global viewport settings or restore the default BareBones config files that may have previously been stripped out?" \
+        $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT \
+        "1" "| Strip the bezels from the config files" \
+        "2" "| Restore the default BareBones config files" \
+        3>&1 1>&2 2>&3)
+    ;;
+    2 ) #4:3"
+      displaygroupname="DMT"
+      displaygroup="2"
+      displaymode="16"
+      newoverlay=$overlay43
+      viewportX="9"; viewportY="9"; viewportW="1006"; viewportH="750"
+      newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/4x3"
+    ;;
+    3 ) #5:4"
+      displaygroupname="DMT"
+      displaygroup="2"
+      displaymode="35"
+      newoverlay=$overlay54
+      viewportX="12"; viewportY="41"; viewportW="1256"; viewportH="942"
+                       stretchY="12";                    stretchH="1000"
+      newcfgfolder="/home/pi/srfoverlays/Sinden_BB_screenratio-main/5x4"
+
+      stretchchoice=$(whiptail --title "$title" --menu \
+        "\nAlmost all of the games available are made with the 4:3 aspect ratio in mind. The display you have selected has a slightly narrower (taller) aspect ratio.\n\nWould you like to:" \
+        $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT \
+        "1" "| Display the games in their true 4:3 aspect ratio" \
+        "2" "| Distort the games so they stretch to fill the whole screen?" \
+        3>&1 1>&2 2>&3)
+
+      case $stretchchoice in
+        1 )
+          echo " : Excellent choice."
+        ;;
+        2 )
+          echo " : Distort the image."
+          viewportY=$stretchY
+          viewportH=$stretchH
+        ;;
+        * )
+          echo "  : Cancelled"
+          exit 0
+        ;;
+      esac
+    ;;
+    * )
+      echo "  : Cancelled"
+      exit 0
+    ;;
+  esac
+
 }
 
 
@@ -158,6 +161,7 @@ function displaymodes2(){
       echo $configtxt " updated."
     else
       echo "Your display doesn't support the required display mode. Cancelling"
+      TERM=ansi whiptail --title "$title" --infobox "Your display doesn't support the required display mode... Cancelling\nMake sure your Pi is connected to a display and that it is turned on before trying again" $WT_HEIGHT $WT_WIDTH
       exit
     fi    
 }
@@ -173,36 +177,38 @@ function restoreOG(){
 }
 
 
+
+function ratioquestion(){
+  ratiochoice=$(whiptail --title "$title" --nocancel --menu \
+    "$1" \
+    $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT \
+    "1" "| Correct ratio" \
+    "2" "| Stretch to fill" \
+    3>&1 1>&2 2>&3)
+}
+
+
 function trueratio(){
-  colourecho $_cQUERY "Would you like games that are natively 4:3 to be displayed in their correct ratio or stretched to fill the screen?"
-  colourecho $_cOPTIONS "[1] Correct ratio; or"
-  colourecho $_cOPTIONS "[2] Stretch to fill."
-  while true; do
-    read -N1 ratiochoice
-    case $ratiochoice in
-      1 )
-	echo " : Correct ratio"
-        cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/bombbee.cfg"
-        cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/cutieq.cfg"
-        cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/geebee.cfg"
-        cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/mmagic.cfg"
-        cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/bronx.cfg"
-        break
-      ;;
-      2 )
-	echo " : Stretch to fill"
-        break
-      ;;
-      * )
-        echo " : Invalid choice"
-      ;;
+  ratioquestion "\nWould you like games that are natively 3:4 to be displayed in their correct ratio or distorted to 4:3?"
+  case $ratiochoice in
+    1 )
+      echo " : Correct ratio"
+      cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/bombbee.cfg"
+      cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/cutieq.cfg"
+      cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/geebee.cfg"
+      cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/mmagic.cfg"
+      cp -v $newcfgfolder"/3x4.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/bronx.cfg"
+      break
+    ;;
+    2 )
+      echo " : Stretch to fill"
+      break
+    ;;
+    * )
+      echo " : Invalid choice"
+    ;;
     esac
-  done
-  colourecho $_cQUERY "Would you like Cybertank, which is natively 8:3 to be displayed in its correct ratio or stretched to fill the screen?"
-  colourecho $_cOPTIONS "[1] Correct ratio; or"
-  colourecho $_cOPTIONS "[2] Stretch to fill."
-  while true; do
-    read -N1 ratiochoice
+  ratioquestion "\nWould you like Cybertank, which is natively 8:3, to be displayed in its correct ratio or distorted 4:3?"
     case $ratiochoice in
       1 )
 	echo " : Correct ratio"
@@ -217,12 +223,7 @@ function trueratio(){
         echo " : Invalid choice"
       ;;
     esac
-  done
-  colourecho $_cQUERY "Would you like Razzmatazz, which is natively 7:8 to be displayed in its correct ratio or stretched to fill the screen?"
-  colourecho $_cOPTIONS "[1] Correct ratio; or"
-  colourecho $_cOPTIONS "[2] Stretch to fill."
-  while true; do
-    read -N1 ratiochoice
+  ratioquestion "\nWould you like Razzmatazz, which is natively 7:8 to be displayed in its correct ratio or distorted to 4:3?"
     case $ratiochoice in
       1 )
 	echo " : Correct ratio"
@@ -237,12 +238,7 @@ function trueratio(){
         echo " : Invalid choice"
       ;;
     esac
-  done
-  colourecho $_cQUERY "Would you like the Tic-80 system, which is natively 16:9 to be displayed in its correct ratio or stretched to fill the screen?"
-  colourecho $_cOPTIONS "[1] Correct ratio; or"
-  colourecho $_cOPTIONS "[2] Stretch to fill."
-  while true; do
-    read -N1 ratiochoice
+  ratioquestion "\nWould you like the Tic-80 system, which is natively 16:9 to be displayed in its correct ratio or distorted to 4:3?"
     case $ratiochoice in
       1 )
 	echo " : Correct ratio"
@@ -257,29 +253,26 @@ function trueratio(){
         echo " : Invalid choice"
       ;;
     esac
-  done
 }
 
 
 function gollyghost43() {
   cd "/home/pi/srfoverlays/Sinden_BB_screenratio-main/GGBT"
-  colourecho $_cQUERY "Would you like to remove the 16:9 scoreboard from Golly Ghost and Bubble Trouble, and apply calibrations for these games suited to a 4:3 display? (y/n)"
-  colourecho $_cQUERY "Make your selection"
-  read -N1 yn
-  case $yn in
-    y|Y )
+  if (whiptail  --title "$title" --yesno \
+    "Would you like to remove the 16:9 scoreboard from Golly Ghost and Bubble Trouble, and apply calibrations for these games suited to a 4:3 display?" \
+    $WT_HEIGHT $WT_WIDTH \
+    3>&1 1>&2 2>&3)
+    then
       echo " : Remove scoreboard"
       cp -v gollygho.cfg.43 /home/pi/RetroPie/roms/arcade/Lightgun_Games/mame/cfg/gollygho.cfg
       cp -v nvram.gg43 /home/pi/RetroPie/roms/arcade/Lightgun_Games/mame/nvram/gollygho/nvram
       cp -v bubbletr.cfg.43 /home/pi/RetroPie/roms/arcade/Lightgun_Games/mame/cfg/bubbletr.cfg
       cp -v nvram.bt43 /home/pi/RetroPie/roms/arcade/Lightgun_Games/mame/nvram/bubbletr/nvram
-    ;;
-    * )
+    else
       echo " : Keep Scoreboard"
       cp -v $newcfgfolder"/16x9.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/gollygho.cfg"
       cp -v $newcfgfolder"/16x9.cfg" "/opt/retropie/configs/all/retroarch/config/MAME/bubbletr.cfg"
-    ;;
-  esac
+    fi
   cd /home/pi
 }
 
@@ -328,57 +321,53 @@ function main() {
     echo "ERROR: script must be run with sudo"
     exit 0
   else
-    colourecho $_cQUERY "This script requires an internet connection."
-    colourecho $_cQUERY "This script will make significant changes to your system.  Please ensure you have made a backup of your system before continuing."
-    colourecho $_cOPTIONS "-Are you sure you wish to proceed? (y/n)"
-    read -N1 yn
-    case $yn in
-      y|Y )
-        echo " : OK..."
-        displaymodes
-	case $restorechoice in
-	  1 )
-            echo " : Replace"
-            displaymodes2
-            dloverlays
-            replacerefs
-            gollyghost43
-            trueratio 
-          ;;
-          2 )
-            echo " : Restore"
-            displaymodes2
-            restoreOG
-            dloverlays
-            gollyghost169
-          ;;
-          * )
-            echo " : Cancelled"
-            exit
-          ;;
-        esac
-      ;;
-      * )
-        echo " : Cancelled"
-        exit
-      ;;
-    esac
+
+  if (whiptail --fb --title "$title" --yesno \
+    "This script requires an internet connection.\n\nThis script will make significant changes to your system. Please ensure you have made a backup of your system before continuing.\n\nAre you sure you wish to proceed?" \
+    $WT_HEIGHT $WT_WIDTH )
+    then
+      echo " : OK..."
+      displaymodes
+       case $restorechoice in
+	1 )
+          echo " : Replace"
+          displaymodes2
+          dloverlays
+          replacerefs
+          gollyghost43
+          trueratio 
+        ;;
+        2 )
+          echo " : Restore"
+          displaymodes2
+          restoreOG
+          dloverlays
+          gollyghost169
+        ;;
+        * )
+          echo " : Cancelled"
+          exit
+        ;;
+      esac
+    else
+      echo " : Cancelled"
+      exit
+    fi
+
     tidyup "/home/pi/srfoverlays"
     chown -R pi:pi /opt/retropie/configs/
     chown -R pi:pi /home/pi/
-    colourecho $_cQUERY "Process completed.  You should now reboot your Pi for the the changes to take effect."
-    colourecho $_cOPTIONS "Do you want to reboot now? (y/n)"
-    read -N1 yn
-    case $yn in
-      y|Y )
-        colourecho "cLRED" " : REBOOTING"
-        reboot
-      ;;
-      * )
+
+  if (whiptail --fb --title "$title" --yesno \
+    "Process completed.  You should now reboot your Pi for the the changes to take effect.\n\nDo you want to reboot now?" \
+    $WT_HEIGHT $WT_WIDTH )
+    then
+      colourecho "cLRED" " : REBOOTING"
+      reboot
+    else
         colourecho "cLRED" " : Don't forget to reboot!"
-      ;;
-    esac
     fi
+  fi
 }
 
 main
